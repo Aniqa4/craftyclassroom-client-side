e8b3375fdc9c4057a3c4db15a61937da6af79652
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-06-08 04:32:07 +0600

    added ui design of all instructor and all classes route

--- src/Components/Classes/Classes.jsx
+++ src/Components/Classes/Classes.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
+import SectionTitle from '../SectionTitle/SectionTitle';
+import slider4 from '../../assets/Slider/slider4.jpg';
 
 function Classes() {
   return (
-    <div className=' container mx-auto my-24'>Classes</div>
+    <div className=' container mx-auto my-24'>
+      <SectionTitle title={'All Classes'}></SectionTitle>
+      <div className='grid grid-cols-4 gap-20'>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={slider4} className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions">
+            <button className="btn bg-gray-200 text-cyan-950">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={slider4} className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions">
+            <button className="btn bg-gray-200 text-cyan-950">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={slider4} className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions">
+            <button className="btn bg-gray-200 text-cyan-950">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={slider4} className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions">
+            <button className="btn bg-gray-200 text-cyan-950">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Instructor/Instructor.jsx
+++ src/Components/Instructor/Instructor.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import slider4 from '../../assets/Slider/slider4.jpg';
+import SectionTitle from '../SectionTitle/SectionTitle';
 
 function Instructor() {
   return (
-    <div className=' container mx-auto my-24'>Instructor</div>
+    <div className='container mx-auto my-24'>
+      <SectionTitle title={'All Instructors'}></SectionTitle>
+      <div className='grid grid-cols-2 gap-20'>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure><img src={slider4} className='' /></figure>
+          <div className="card-body">
+            <h1>Name:</h1>
+            <p>Email:</p>
+            <div className="card-actions justify-end">
+              <button className="btn bg-gray-200 text-cyan-950 font-bold">See Classes</button>
+            </div>
+          </div>
+        </div>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure><img src={slider4} /></figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure><img src={slider4} /></figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
