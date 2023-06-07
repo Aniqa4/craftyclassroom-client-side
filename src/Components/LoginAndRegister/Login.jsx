daeccc1f32e391ea02ad9621fbb9e21aa50a4ebd
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-06-08 02:05:29 +0600

    added sign page ui

--- src/Components/LoginAndRegister/Login.jsx
+++ src/Components/LoginAndRegister/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SectionTitle from '../SectionTitle/SectionTitle';
 import { FcGoogle } from 'react-icons/fc';
 import {AiFillEyeInvisible} from 'react-icons/ai'
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
@@ -10,16 +11,17 @@ function Login() {
       <div className=' container mx-auto w-4/12 border rounded-xl p-20 bg-gray-100'>
         <form className=' py-5'>
           <label> Email:</label>
-          <input type="text" placeholder="Your Email Address" className="input input-bordered w-full max-w-xs" /><br />
+          <input type="text" placeholder="Your Email Address" className="input input-bordered w-full" /><br />
           <label> Password:</label>
           <div className='flex relative'>
-            <input type="password" placeholder="Your Password" className="input input-bordered w-full max-w-xs mb-2" /> 
-            <span className=' absolute right-10 top-1/4'><AiFillEyeInvisible/></span>
+            <input type="password" placeholder="Your Password" className="input input-bordered w-full mb-2" /> 
+            <span className=' absolute right-2 top-1/4'><AiFillEyeInvisible/></span>
           </div><br />
-          <input type="submit" value="Log In" className="input input-bordered w-full max-w-xs" />
+          <input type="submit" value="Log In" className="input input-bordered w-full" />
         </form>
         <div className="divider">OR</div>
-        <button className='flex items-center gap-2 text-xl mx-auto border rounded-full bg-white p-2 px-5'>Log In with Google<FcGoogle /></button>
+        <button className='flex items-center gap-2 text-xl mx-auto border rounded-full bg-white py-2 px-5'>Log In with Google<FcGoogle /></button>
+        <p className=' text-center mt-10'>Don't have an account? <Link to="/register"><span className='underline'>Register</span></Link></p>
       </div>
     </div>
   )
--- src/Components/LoginAndRegister/Register.jsx
+++ src/Components/LoginAndRegister/Register.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import SectionTitle from '../SectionTitle/SectionTitle';
+import { FcGoogle } from 'react-icons/fc';
+import { AiFillEyeInvisible } from 'react-icons/ai'
+import { Link } from 'react-router-dom';
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='container mx-auto my-24 '>
+      <SectionTitle title={'Sign up to CraftyClassroom'}></SectionTitle>
+      <div className=' container mx-auto w-5/12 border rounded-xl p-20 bg-gray-100'>
+        <form className=' py-5'>
+          <label> Name:</label>
+          <input type="text" placeholder="Enter Your Name" className="input input-bordered w-full" />
+          <label> Email:</label>
+          <input type="email" placeholder="Enter Your Email Address" className="input input-bordered w-full" />
+          <label> Password:</label>
+          <div className='flex relative'>
+            <input type="password" placeholder="Enter Your Password" className="input input-bordered w-full" />
+            <span className=' absolute right-2 top-1/4'><AiFillEyeInvisible /></span>
+          </div>
+          <label>Confirm Password:</label>
+          <div className='flex relative'>
+            <input type="password" placeholder="Confirm Password" className="input input-bordered w-full" />
+            <span className=' absolute right-2 top-1/4'><AiFillEyeInvisible /></span>
+          </div>
+          <label>PhotoURL:</label>
+          <input type="photo" placeholder="Enter Your PhotoURL" className="input input-bordered w-full" />
+          <label>Gender:</label>
+          <input type="text" placeholder="Enter Your Gender" className="input input-bordered w-full" />
+          <label>Phone Number:</label>
+          <input type="number" pattern="[0-9]*" placeholder="Enter Your Phone number" className="input input-bordered w-full" />
+          <label>Address:</label>
+          <input type="address" placeholder="Enter Your Address" className="input input-bordered w-full" />
+          <input type="submit" value="Sign up" className="input input-bordered w-full mt-5" />
+        </form>
+        <p className=' text-center mt-10'>Already have an account? <Link to="/login"><span className='underline'>Log In</span></Link></p>
+      </div>
+    </div>
   )
 }
 
