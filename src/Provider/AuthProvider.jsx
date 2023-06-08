82cc4c00fd4c1b9f03502c8203f2dffbfb5ff800
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-06-08 17:27:19 +0600

    added authContext

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,62 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import app from '../../Firebase/firebase.config';
+
+
+const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    const signUp=(email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const signIn=(email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const googleSignIn=(provider)=>{
+        setLoading(true);
+        return signInWithPopup(auth,provider);
+    }
+
+    const logOut=()=>{
+        setLoading(true);
+        return signOut (auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            console.log('logged in user inside auth state observer', loggedUser);
+            setUser(loggedUser);
+            setLoading(false);
+        })
+
+        return () => {
+            unsubscribe()
+        }
+    })
+    const authInfo = {
+        user,
+        signUp,
+        signIn,
+        googleSignIn,
+        logOut,
+        loading
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export { AuthContext, AuthProvider,auth };
