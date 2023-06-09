b4b594aaad25420518b56a6fca2dabcf07498bef
Aniqa Chowdhury <aanika.chowdhury04@gmail.com>
2023-06-09 22:39:03 +0600

    completed admin dashboard ui

--- src/Components/Dashboard/AdminDashboard.jsx
+++ src/Components/Dashboard/AdminDashboard.jsx
@@ -15,14 +15,15 @@ function AdminDashboard() {
   }, [])
 
   useEffect(() => {
-    fetch('http://localhost:5000/classes')
+    fetch('http://localhost:5000/allclasses')
       .then(res => res.json())
       .then(data => {
         setClasses(data)
       })
   }, [])
 
-  console.log(users);
+
+  console.log(classes);
 
   return (
     <div className='container mx-auto my-24'>
@@ -65,17 +66,24 @@ function AdminDashboard() {
             </TabPanel>
             <TabPanel>
               <div >
+                <SectionTitle title={'All Classes'}></SectionTitle>
+                <div className='grid grid-cols-3 gap-10'>
                 {
-                  classes.map((singleClass,index)=>
-                  <div key={index} className='grid grid-cols-2 mb-5'>
-                    {
-                      singleClass.classes.map((x,index)=>
-                      <div key={index}>
-                        <img src={x.classImage} className='w-96 h-60 rounded-xl'/>
-                      </div>)
-                    }
-                  </div>
-                )}
+                  classes.map(singleClass=>
+                  <div key={singleClass._id} className='p-10 border rounded shadow-xl'>
+                      <img src={singleClass.classImage} className='w-96 h-56 rounded-xl mb-2'/>
+                      <h1>Class Name: {singleClass.className}</h1>
+                      <p>Instructor Name : {singleClass.name}</p>
+                      <p>Instructor email : {singleClass.email}</p>
+                      <p>Available Seats : {singleClass.availableSeats}</p>
+                      <p>price : {singleClass.price}</p>
+                      <p>status : {singleClass.status}</p>
+                      <button className='btn bg-cyan-200 text-cyan-950 mt-2'>Approve</button>
+                      <button className='btn bg-cyan-200 text-cyan-950'>Deny</button>
+                      <button className='btn bg-cyan-200 text-cyan-950'>Feedback</button>
+                  </div>)
+                }
+                </div>
               </div>
             </TabPanel>
           </div>
